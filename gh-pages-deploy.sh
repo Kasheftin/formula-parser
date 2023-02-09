0dbe96b8f2ae2352be4dc21a734c4f633f56daa0
Kasheftin <Kasheftin@gmail.com>
2023-02-10 00:49:24 +0200

    tmp commit, react base removed

--- gh-pages-deploy.sh
+++ gh-pages-deploy.sh
@@ -4,9 +4,29 @@ cd vue
 npm i
 npm run build
 echo "Vue Build Completed"
+
 cd ..
+
 echo "React Build Started"
 cd react
 npm i
 npm run build
 echo "React Build Completed"
+
+cd ..
+
+mkdir dist
+mv vue/dist dist
+mv dist/dist dist/vue
+mv react/dist/react-vite dist
+mv react-vite react
+rmdir react/dist
+
+git checkout --orphan gh-pages
+git --work-tree dist add --all
+git --work-tree dist commit -m gh-pages
+git push origin HEAD:gh-pages --force
+
+rm -rf dist
+git checkout -f master
+git branch -D gh-pages
